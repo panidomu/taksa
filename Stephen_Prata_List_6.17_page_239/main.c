19327425183cd4ce7ea708c22ca07d956b1bf08b
panidomu <panidomunn@gmail.com>
2016-10-07 23:37:01 +0300

    ver2

--- Stephen_Prata_List_6.17_page_239/main.c
+++ Stephen_Prata_List_6.17_page_239/main.c
@@ -1,16 +1,16 @@
 //It is example from book by Stephen Prata
-//List 6.17 page 239_matrioshka
+//List 6.18 page 240_matrioshka
 
 #include <stdio.h>
 #include <stdlib.h>
-#define ROWS 6
+#define ROWS 10
 #define CHARS 10
 int main()
 {
     int row;
     char ch;
     for (row=0; row<ROWS; row++)
-    {for (ch='A'; ch<('A'+CHARS); ch++)
+    {for (ch='A'+row; ch<('A'+CHARS); ch++)
         printf("%c", ch);
     printf("\n");
     }
