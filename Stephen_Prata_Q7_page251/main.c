00f0b020d474dfb70f49f51a3f20f2f5b1173d47
panidomu <panidomunn@gmail.com>
2016-10-18 21:42:33 +0300

    Stephen_Prata_Q7_page251

---
+++ Stephen_Prata_Q7_page251/main.c
@@ -0,0 +1,16 @@
+//Print
+//Hi! Hi! Hi! Bye! Bye! Bye! Bye! Bye!
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 0 ;
+while ( ++ i < 4 )
+printf( "Hi! " ) ;
+do
+printf ( "Bye! " ) ;
+while ( i ++ < 8 ) ;
+
+}
