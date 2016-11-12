8f04257aa536e72d3eed8d576c33805e552c42b2
panidomu <panidomunn@gmail.com>
2016-11-12 18:46:38 +0300

    Add Stephen Prata Q15 page 257

---
+++ Stephen_Prata_Q15_page257/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define InitialContributionDaphne 100
+#define InitialContributionDeirdre 100
+#define DaphnePercentage 10
+#define DeirdrePercentage 5
+
+
+int main()
+{int NumberOfYears=1;
+ int SumDaphne;
+ int SumDeirdre;
+ int RiseDaphne;
+ int RiseDeirdre;
+
+ do
+ {
+    NumberOfYears=NumberOfYears+1;
+
+    RiseDaphne=InitialContributionDaphne*DaphnePercentage/100*NumberOfYears;
+    SumDaphne=InitialContributionDaphne+RiseDaphne;
+
+    RiseDeirdre=(InitialContributionDeirdre+RiseDeirdre)*DeirdrePercentage/100*NumberOfYears;
+    SumDeirdre=InitialContributionDeirdre+RiseDeirdre;
+
+    NumberOfYears=NumberOfYears+1;
+
+ }
+ while(SumDeirdre<SumDaphne);
+
+    printf("Sum Daphne is %d $\n",SumDaphne);
+    printf("Sum Deirdre is %d $\n",SumDeirdre);
+    printf("This will happen after %d years \n",NumberOfYears);
+    return 0;
+}
